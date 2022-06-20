576963f6f7bec74123c5f6a087908e951c5513b7
yanghuiming77777 <yanghuiming77777@yeah.net>
2022-06-20 20:51:55 +0800

    creatr bixiejianfa

---
+++ src/main/java/com/atguigu/git/wg/BiXiJianFa.java
@@ -0,0 +1,14 @@
+package com.atguigu.git.wg;
+
+
+
+public class BiXiJianFa {
+    public static void main(String[] args) {
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+    }
+}
