c0c16093c65c2bfed0ac65e08103c0aa140215c7
yanghuiming <yanghuiming@yhm.com>
2022-06-20 21:09:41 +0800

    create dugujiujian

---
+++ src/main/java/com/atguigu/git/wg/DuGu.java
@@ -0,0 +1,12 @@
+package com.atguigu.git.wg;
+
+
+public class DuGu {
+    public static void main(String[] args) {
+        System.out.println("ashdashdkjahsda");
+        System.out.println("ashdashdkjahsda");
+        System.out.println("ashdashdkjahsda");
+        System.out.println("ashdashdkjahsda");
+        System.out.println("ashdashdkjahsda");
+    }
+}
